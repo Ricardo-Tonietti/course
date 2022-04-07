bd67b83099edbce2d43f39ff74182e9dc7601520
Ricardo Tonietti <rictonietti@gmail.com>
2022-04-07 17:04:53 -0300

    configuration Eureka client, ajust, pom and yaml

--- src/main/java/com/ead/course/CourseApplication.java
+++ src/main/java/com/ead/course/CourseApplication.java
@@ -2,11 +2,13 @@ package com.ead.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableWebMvc
 @SpringBootApplication
+@EnableEurekaClient
 public class CourseApplication {
 
 	public static void main(String[] args) {
