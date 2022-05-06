b80f944eb2585941b910ce5892bc779327d24591
Ricardo Tonietti <rictonietti@gmail.com>
2022-05-06 11:46:57 -0300

    Spring Security Authentication

---
+++ src/main/java/com/ead/course/specifications/SpecificationTemplate.java
@@ -0,0 +1,78 @@
+package com.ead.course.specifications;
+
+import com.ead.course.models.CourseModel;
+import com.ead.course.models.UserModel;
+import com.ead.course.models.LessonModel;
+import com.ead.course.models.ModuleModel;
+import net.kaczmarzyk.spring.data.jpa.domain.Equal;
+import net.kaczmarzyk.spring.data.jpa.domain.Like;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.And;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
+import org.springframework.data.jpa.domain.Specification;
+
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.Root;
+import java.util.Collection;
+import java.util.UUID;
+
+public class SpecificationTemplate {
+
+    @And({
+            @Spec(path = "courseLevel", spec = Equal.class),
+            @Spec(path = "courseStatus", spec = Equal.class),
+            @Spec(path = "name", spec = Like.class)
+    })
+    public interface CourseSpec extends Specification<CourseModel> {}
+
+    @And({
+            @Spec(path="email", spec= Like.class),
+            @Spec(path = "fullName", spec = Like.class),
+            @Spec(path="userStatus", spec= Equal.class),
+            @Spec(path="userType", spec= Equal.class)})
+    public interface UserSpec extends Specification<UserModel> {}
+
+    @Spec(path = "title", spec = Like.class)
+    public interface ModuleSpec extends Specification<ModuleModel> {}
+
+    @Spec(path = "title", spec = Like.class)
+    public interface LessonSpec extends Specification<LessonModel> {}
+
+    public static Specification<ModuleModel> moduleCourseId(final UUID courseId) {
+        return (root, query, cb) -> {
+            query.distinct(true);
+            Root<CourseModel> course = query.from(CourseModel.class);
+            Expression<Collection<ModuleModel>> coursesModules = course.get("modules");
+            return cb.and(cb.equal(course.get("courseId"), courseId), cb.isMember(root, coursesModules));
+        };
+    }
+
+    public static Specification<LessonModel> lessonModuleId(final UUID moduleId) {
+        return (root, query, cb) -> {
+            query.distinct(true);
+            Root<ModuleModel> module = query.from(ModuleModel.class);
+            Expression<Collection<LessonModel>> moduleLessons = module.get("lessons");
+            return cb.and(cb.equal(module.get("moduleId"), moduleId), cb.isMember(root, moduleLessons));
+        };
+    }
+
+    public static Specification<UserModel> userCourseId(final UUID courseId) {
+        return (root, query, cb) -> {
+            query.distinct(true);
+            Root<CourseModel> course = query.from(CourseModel.class);
+            Expression<Collection<UserModel>> coursesUsers = course.get("users");
+            return cb.and(cb.equal(course.get("courseId"), courseId), cb.isMember(root, coursesUsers));
+        };
+    }
+
+    public static Specification<CourseModel> courseUserId(final UUID userId) {
+        return (root, query, cb) -> {
+            query.distinct(true);
+            Root<UserModel> user = query.from(UserModel.class);
+            Expression<Collection<CourseModel>> usersCourses = user.get("courses");
+            return cb.and(cb.equal(user.get("userId"), userId), cb.isMember(root, usersCourses));
+        };
+    }
+
+
+}
